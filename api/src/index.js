40416900ef611f37d19d41f88b51db96e835746d
RogerMKobus <roger.kobus@gmail.com>
2021-01-04 21:56:25 -0300

    Estrutura inicial criada

---
+++ api/src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const routes = require('./routes');
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(process.env.PORT);
+
+console.log(`OK porta ${process.env.PORT}`)
