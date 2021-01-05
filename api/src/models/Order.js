f9efad6dda6da79ea2434eb52e51600ea0b2b27b
RogerMKobus <roger.kobus@gmail.com>
2021-01-05 12:07:06 -0300

    Adicionando relação entre lane e order

--- api/src/models/Order.js
+++ api/src/models/Order.js
@@ -1,10 +1,11 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, ObjectId } = require('mongoose');
 
 const OrderSchema = new Schema({
     
-    title: { type: String, required: true, trim: true, index: true },
-    contact: { type: String, required: true, trim: true, index: true },
-    description: { type: String, required: true, trim: true, index: true },
+    title: { type: String, required: true },
+    contact: { type: String, required: false },
+    description: { type: String, required: false },
+    lane: { type: ObjectId, ref: 'Lane', require: true, default: '5ff47cefac74864154efcc0e' },
     
 },
     {
