54026ee7046c31b5577f2b12c759798be73cb31e
RogerMKobus <roger.kobus@gmail.com>
2021-01-11 19:41:33 -0300

    Adicionando conexão com a api

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 mongoose.connect(`${process.env.MONGOURI}`);
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.listen(process.env.PORT);
--- frontend/src/components/main/index.js
+++ frontend/src/components/main/index.js
@@ -1,14 +1,31 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import api from '../../services/api'
 import { Container, Content } from './styles';
 
-export default class Header extends React.Component{
+export default class Header extends Component{
+
+    state = {
+        orders: []
+    }
+
+    async componentDidMount() {
+        const { data: orders } = await api.get('/')
+
+        this.setState({ orders })
+    }
 
     render(){
         return (
             <Container>
                 <Content>
-                    <h1>Content</h1>
+                    { this.state.orders.map(order => 
+                    <h1 key={order.id}>
+                        {order.title}
+                        <p>
+                            {order.description}
+                        </p>
+                    </h1>
+                    ) }
                 </Content>
             </Container>
         );
