457caa839915160a07a9e9d1ca9d27b47c87c2f7
RogerMKobus <roger.kobus@gmail.com>
2021-03-01 17:04:09 -0300

    Corrigindo problema que expandia todos os Accordions juntos

--- mobile/src/components/Card/index.js
+++ mobile/src/components/Card/index.js
@@ -5,7 +5,7 @@ import { List, IconButton } from 'react-native-paper';
 import { StyledText } from './styles';
 import { useNavigation } from '@react-navigation/native';
 
-const Card = (laneId ) => {
+const Card = (laneId) => {
 
   const navigation = useNavigation();
 
@@ -28,39 +28,43 @@ const Card = (laneId ) => {
       data={orders}
       renderItem={({ item }) => {
         if (item.lane === laneId.laneId) {
-          return <List.Accordion titleStyle={{ fontSize: 18 }}
-          title={item.title}
-            expanded={expanded}
-            onPress={handlePress}>
-            {
-              item.description ? <>
-                <StyledText>Descrição:</StyledText>
-                <List.Item titleStyle={{ fontSize: 18 }}
-                title={item.description} />
-              </>
-                :
-                <></>
-            }
-            {
-              item.contact ? <>
-                <StyledText>Contato:</StyledText>
-                <List.Item titleStyle={{ fontSize: 18 }}
-                title={item.contact} />
-              </>
-                :
-                <></>
-            }
-            <StyledText>Data:</StyledText>
-            <List.Item titleStyle={{ fontSize: 18 }}
-            title={item.date} />
-            <IconButton icon="playlist-edit"
-            size={30}
-            onPress={()=> navigation.navigate('Edit')}
-            />
-            <IconButton icon="delete" 
-            size={30}
-            style={{ position: 'absolute', left: '15%', top: '80%' }}/>
-          </List.Accordion>
+          return <List.AccordionGroup>
+            <List.Accordion
+              id={item._id}
+              titleStyle={{ fontSize: 18 }}
+              title={item.title}
+              expanded={expanded}
+              onPress={handlePress}>
+              {
+                item.description ? <>
+                  <StyledText>Descrição:</StyledText>
+                  <List.Item titleStyle={{ fontSize: 18 }}
+                    title={item.description} />
+                </>
+                  :
+                  <></>
+              }
+              {
+                item.contact ? <>
+                  <StyledText>Contato:</StyledText>
+                  <List.Item titleStyle={{ fontSize: 18 }}
+                    title={item.contact} />
+                </>
+                  :
+                  <></>
+              }
+              <StyledText>Data:</StyledText>
+              <List.Item titleStyle={{ fontSize: 18 }}
+                title={item.date} />
+              <IconButton icon="playlist-edit"
+                size={30}
+                onPress={() => navigation.navigate('Edit')}
+              />
+              <IconButton icon="delete"
+                size={30}
+                style={{ position: 'absolute', left: '15%', top: '80%' }} />
+            </List.Accordion>
+          </List.AccordionGroup>
         }
       }
       }
