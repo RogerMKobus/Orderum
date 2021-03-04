bc2eb5aec76a5affee801a1de1427dd70e0f2c96
RogerMKobus <roger.kobus@gmail.com>
2021-03-04 16:38:07 -0300

    Arrumando problema com datas

--- frontend/src/components/Form/DatePicker.jsx
+++ frontend/src/components/Form/DatePicker.jsx
@@ -7,6 +7,7 @@ import "react-datepicker/dist/react-datepicker.css";
 
 
 function DatePicker({ edit, name, ...rest }) {
+    
     const dateRef = useRef(null);
     const { fieldName, registerField } = useField(name);
 
@@ -14,28 +15,28 @@ function DatePicker({ edit, name, ...rest }) {
 
     const [date, setDate] = useState();
 
-    function returnDate() {
-        if (edit) {
-            const dd = location.state.order.date.slice(0, 2)
-            const mm = location.state.order.date.slice(3, 5)
-            const yy = location.state.order.date.slice(6, 10)
-            const newDate = new Date(`${yy}-${mm}-${dd}T00:00:00`)
-            setDate(newDate)
-        }
-        else {
-            const newDate = new Date();
-            setDate(newDate.getDate() + 7)
-        }
-    }
-
     useEffect(() => {
+        function returnDate() {
+            if (edit) {
+                const parts = location.state.order.date.split('/')
+                if(parts[1].length === 1){
+                    parts[1] = (0 + parts[1])
+                }
+                const newDate = new Date(`${parts[2]}-${parts[1]}-${parts[0]}T00:00:00`)
+                setDate(newDate)
+            }
+            else {
+                const newDate = new Date().setDate(new Date().getDate() + 7)
+                setDate(newDate)
+            }
+        }
         returnDate()
         registerField({
             name: fieldName,
             ref: dateRef.current,
             path: 'props.selected'
         })
-    }, [fieldName, registerField])
+    }, [fieldName, registerField, edit, location.state])
 
     return (
         <ReactDatePicker
